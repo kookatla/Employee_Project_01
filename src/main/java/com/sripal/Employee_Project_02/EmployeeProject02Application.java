57faa601f053099405075ada79247267ce745e25
kookatla <kookatlasripalyadav.25@gmail.com>
2023-11-05 13:38:56 +0530

    this is first commit

---
+++ src/main/java/com/sripal/Employee_Project_02/EmployeeProject02Application.java
@@ -0,0 +1,13 @@
+package com.sripal.Employee_Project_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProject02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sripal/Employee_Project_02/EmployeeProject02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sripal.Employee_Project_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeProject02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
